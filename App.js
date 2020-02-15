2676388b2136de86c8c5f4b800fb6ced05ed431a
Bruno Parga <brunoparga@gmail.com>
2020-02-15 19:46:54 -0300

    Play with layout

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { View } from 'react-native';
 export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'column'}}>
+      <View style={{
+        flex: 1, 
+        flexDirection: 'column', 
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+      }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
