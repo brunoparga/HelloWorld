44e4666091ebdee02d9fb573e9164433b63d33e1
Bruno Parga <brunoparga@gmail.com>
2020-02-15 18:35:50 -0300

    Do new component props

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default class Bananas extends Component {
+class Greeting extends Component {
   render() {
-    const pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center', top: 230}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
     );
   }
 }
