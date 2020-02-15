d49e79bbf7875cd3bceccb84348f88666b2cf4d1
Bruno Parga <brunoparga@gmail.com>
2020-02-15 19:58:14 -0300

    Do TextInput🍕

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' }
+  }
+
   render() {
     return (
-      <View style={{
-        flex: 1, 
-        flexDirection: 'column', 
-        justifyContent: 'space-evenly',
-        alignItems: 'center'
-      }}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      <View style={{padding: 10, top: 250}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
       </View>
     );
   }
