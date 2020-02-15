31dbc693fac8d73f59fc4c21301a41eb2a96e638
Bruno Parga <brunoparga@gmail.com>
2020-02-15 18:50:31 -0300

    Do Blink

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+
+  componentDidMount(){
+    setInterval(() => this.setState((previousState) => (
+      { textColor: previousState.textColor === 'black' ? 'white' : 'black' }
+    )), 1000);
+  }
+
+  state = { textColor: 'black' };
+
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    );
+      <Text style={{color: this.state.textColor}}>{this.props.text}</Text>
+    )
   }
 }
 
-export default class LotsOfGreetings extends Component {
+export default class BlinkApp extends Component {
   render() {
     return (
       <View style={{alignItems: 'center', top: 230}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
