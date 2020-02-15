4e4ef59d41213a346d02c6657d9bda463ab4a877
Bruno Parga <brunoparga@gmail.com>
2020-02-15 19:17:48 -0300

    Do Flex Direction

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center', top: 250}}>
+      <View style={{flex: 1, flexDirection: 'column'}}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
